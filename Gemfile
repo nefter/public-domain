02d7f88a243a02f386d8e1e3c6818aaca93c56c0
Nef Rossano <nefter@protonmail.com>
2022-01-18 21:31:36 -0600

    Install and prepare devise

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "3.1.0"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
+gem "devise"
 gem "pg", "~> 1.1"
 gem "puma", "~> 5.0"
 gem "rails", "~> 7.0.1"
--- config/application.rb
+++ config/application.rb
@@ -22,15 +22,8 @@ Bundler.require(*Rails.groups)
 
 module PublicDomain
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.middleware.use Rack::Deflater
     config.load_defaults 7.0
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Central Time (US & Canada)"
   end
 end
