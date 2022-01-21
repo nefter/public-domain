400eb07c1acd3dc1b6a36200fe6bc8a15d1b359c
Nef Rossano <nefter@protonmail.com>
2022-01-20 18:21:56 -0600

    Update landing test

--- test/controllers/landing_controller_test.rb
+++ test/controllers/landing_controller_test.rb
@@ -4,7 +4,7 @@ require "test_helper"
 
 class LandingControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get "#landing#index"
+    get "landing"
     assert_response :success
   end
 end
