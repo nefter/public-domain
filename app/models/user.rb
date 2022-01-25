0d75343b0b4824cf54f27db82d1bd28c0060684f
Nef Rossano <nefter@protonmail.com>
2022-01-24 22:36:40 -0600

    DB Adjustments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :trackable, :recoverable, :rememberable, :validatable
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
 end
