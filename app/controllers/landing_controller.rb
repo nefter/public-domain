bc6ccd3146227a5d7c34c28fc95849140429653f
Nef Rossano <nefter@protonmail.com>
2022-01-27 21:41:41 -0600

    Reset and home controller setting

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class LandingController < ApplicationController
-  layout "landing"
+  def index; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   root "landing#index"
+  devise_for :users
 end
