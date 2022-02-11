edfa970fbe03848eb208408fe73d0368da019a77
Nef <286767+nefter@users.noreply.github.com>
2022-02-11 14:30:05 -0600

    Items crud operations (#30)
    * Bump puma from 5.6.0 to 5.6.1
    
    Bumps [puma](https://github.com/puma/puma) from 5.6.0 to 5.6.1.
    - [Release notes](https://github.com/puma/puma/releases)
    - [Changelog](https://github.com/puma/puma/blob/master/History.md)
    - [Commits](https://github.com/puma/puma/compare/v5.6.0...v5.6.1)
    
    ---
    updated-dependencies:
    - dependency-name: puma
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Reset and home controller setting
    
    * Setting of CRUD operations
    
    * Item controller update
    
    * Addition of user and items references
    
    * Updates in user forms
    
    * Updates inf devise forms
    
    * Bump esbuild from 0.14.14 to 0.14.18
    
    Bumps [esbuild](https://github.com/evanw/esbuild) from 0.14.14 to 0.14.18.
    - [Release notes](https://github.com/evanw/esbuild/releases)
    - [Changelog](https://github.com/evanw/esbuild/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/evanw/esbuild/compare/v0.14.14...v0.14.18)
    
    ---
    updated-dependencies:
    - dependency-name: esbuild
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Image proportions adjustments
    
    * Revert "Idea files to gitignore"
    
    This reverts commit 2621468e7c2f4afcc9b5c4dae8e94785e2e39774.
    
    * Bulma assets update
    
    * Changes to test shared PR
    
    * Modal changes
    
    * Login fix
    
    * Navigation ux/ui update
    
    * Adding icons to home modal
    
    * Migration to add username
    
    * Linter corrections
    
    * Updating home view
    
    * Fixed bugs
    
    * Updating home views
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -15,13 +15,13 @@ module Users
     #   super
     # end
 
-    # def sign_up_params
-    #   params.require(:user).permit(:email, :password, :password_confirmation)
-    # end
+    def sign_up_params
+      params.require(:user).permit(:email, :username, :password, :password_confirmation)
+    end
 
-    # def account_update_params
-    #   params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :avatar)
-    # end
+    def account_update_params
+      params.require(:user).permit(:email, :username, :password, :password_confirmation, :current_password, :avatar)
+    end
 
     # GET /resource/edit
     # def edit
