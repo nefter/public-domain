9f25a6719412fa4c60c67c410c29eb9fc53c3734
Nef <286767+nefter@users.noreply.github.com>
2022-02-10 21:14:31 -0600

    Items crud operations (#29)
    * Bump puma from 5.6.0 to 5.6.1
    
    Bumps [puma](https://github.com/puma/puma) from 5.6.0 to 5.6.1.
    - [Release notes](https://github.com/puma/puma/releases)
    - [Changelog](https://github.com/puma/puma/blob/master/History.md)
    - [Commits](https://github.com/puma/puma/compare/v5.6.0...v5.6.1)
    
    ---
    updated-dependencies:
    - dependency-name: puma
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Reset and home controller setting
    
    * Setting of CRUD operations
    
    * Item controller update
    
    * Addition of user and items references
    
    * Updates in user forms
    
    * Updates inf devise forms
    
    * Bump esbuild from 0.14.14 to 0.14.18
    
    Bumps [esbuild](https://github.com/evanw/esbuild) from 0.14.14 to 0.14.18.
    - [Release notes](https://github.com/evanw/esbuild/releases)
    - [Changelog](https://github.com/evanw/esbuild/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/evanw/esbuild/compare/v0.14.14...v0.14.18)
    
    ---
    updated-dependencies:
    - dependency-name: esbuild
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Image proportions adjustments
    
    * Revert "Idea files to gitignore"
    
    This reverts commit 2621468e7c2f4afcc9b5c4dae8e94785e2e39774.
    
    * Bulma assets update
    
    * Changes to test shared PR
    
    * Modal changes
    
    * Login fix
    
    * Navigation ux/ui update
    
    * Adding icons to home modal
    
    * Migration to add username
    
    * Linter corrections
    
    * Updating home view
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,13 @@
 class ApplicationController < ActionController::Base
   def index; end
   protect_from_forgery with: :exception
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: %i[avatar avatar_cache remove_avatar])
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: %i[avatar avatar_cache remove_avatar])
-  #   devise_parameter_sanitizer.permit(:account_update, keys: %i[avatar avatar_cache remove_avatar])
-  # end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[avatar avatar_cache remove_avatar])
+    devise_parameter_sanitizer.permit(:sign_in, keys: %i[avatar avatar_cache remove_avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[avatar avatar_cache remove_avatar])
+  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,87 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Functions to open and close a modal
+    function openModal($el) {
+        $el.classList.add('is-active');
+    }
+
+    function closeModal($el) {
+        $el.classList.remove('is-active');
+    }
+
+    function closeAllModals() {
+        (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+            closeModal($modal);
+        });
+    }
+
+    // Add a click event on buttons to open a specific modal
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+        const modal = $trigger.dataset.target;
+        const $target = document.getElementById(modal);
+        console.log($target);
+
+        $trigger.addEventListener('click', () => {
+            openModal($target);
+        });
+    });
+
+    // Add a click event on various child elements to close the parent modal
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+        const $target = $close.closest('.modal');
+
+        $close.addEventListener('click', () => {
+            closeModal($target);
+        });
+    });
+
+    // Add a keyboard event to close all modals
+    document.addEventListener('keydown', (event) => {
+        const e = event || window.event;
+
+        if (e.keyCode === 27) { // Escape key
+            closeAllModals();
+        }
+    });
+});
+
+// bulmaCarousel
+bulmaCarousel.attach('#carousel-demo', {
+    slidesToScroll: 1,
+    slidesToShow: 6,
+    infinite: true,
+    pagination: false,
+    navigationSwipe: true,
+    // autoplay: true,
+    breakpoints: [{
+        changePoint: 480,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    }, {
+        changePoint: 640,
+        slidesToShow: 2,
+        slidesToScroll: 2
+    }, {
+        changePoint: 768,
+        slidesToShow: 6,
+        slidesToScroll: 6
+    }, {
+        changePoint: 1280,
+        slidesToShow: 6,
+        slidesToScroll: 6
+    }]
+});
+
+// Initialize all div with carousel class
+var carousels = bulmaCarousel.attach('.carousel', options);
+
+// Loop on each carousel initialized
+for (var i = 0; i < carousels.length; i++) {
+    // Add listener to  event
+    carousels[i].on('before:show', state => {
+        console.log(state);
+    });
+}
